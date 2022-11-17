cf867d450b7d84b4d48e16c7649f282ceaf6db04
cnixbtc <111755602+cnixbtc@users.noreply.github.com>
2022-11-17 17:31:34 +0100

    feat: add API info at / route

--- src/app.js
+++ src/app.js
@@ -7,6 +7,24 @@ const port = 3000
 
 app.use('/upcycle', upcycle)
 
+app.get('/', (req, res) => {
+  const html = `
+    <strong>AVAILABLE ROUTES</strong>
+
+    <ul>
+      <li>GET
+        <ul>
+          <li>
+            /upcycle?url=&lt;url&gt;
+          </li>
+        </ul>
+      </li>
+    </ul>
+    `
+  res.set('Content-Type', 'text/html')
+  res.send(html)
+})
+
 app.listen(port, () => {
   console.log(`vat-api: listening on port ${port}`)
 })
